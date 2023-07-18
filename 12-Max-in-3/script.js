d0bf31c490e07530bd94fb5d780cfc95c0fb7f1a
Titus210 <tituskiplagat210@gmail.com>
2023-07-19 00:02:52 +0300

    feat: Finding maximum between three numbers

---
+++ 12-Max-in-3/script.js
@@ -0,0 +1,24 @@
+// get two numbers from user
+var number_one = Number(window.prompt("Enter first number"));
+var number_two = Number(window.prompt("Enter second number"));
+var number_three = Number(window.prompt("Enter third number"));
+
+
+// function to find max
+function findMax(x, y, z) {
+    if (x > y && x > z) {
+        return x;
+    }
+    else if (y > x && y > z)
+        return y;
+    else
+        return z;
+}
+
+var max = String(findMax(number_one, number_two, number_three))
+
+console.log(max)
+
+
+// display to user
+document.getElementById('sum').innerHTML = max;
